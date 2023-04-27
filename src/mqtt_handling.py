a8a70887796b5964919c02766d5de74346418762
EliasWR <eliaswoierefsdal@gmail.com>
2023-04-27 15:40:21 +0200

    Utbedret MQTT

--- src/mqtt_handling.py
+++ src/mqtt_handling.py
@@ -1,28 +1,63 @@
-from flask import Flask, render_template
-from flask import request
-from flask_mqtt import Mqtt
+#from flask import Flask, render_template
+#from flask import request
+#from flask_mqtt import Mqtt
+import paho.mqtt.client as mqtt
 import psycopg2
 from datetime import datetime as dt
 import pandas as pd
 from scipy.signal import butter, filtfilt
 
-app = Flask(__name__)
-app.config['MQTT_BROKER_URL'] = '158.38.67.229'
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = 'ntnu'
-app.config['MQTT_PASSWORD'] = 'ntnuais2103'
-app.config['MQTT_KEEPALIVE'] = 5
-app.config['MQTT_TLS_ENABLED'] = False
-topic1 = 'RTUIot/Ard1/BMP280/Temp'
-topic2 ='NTNU/Miniproj/Elias/FiltTemp'
-topic3 ='RTUIot/flask/data'
+# Setup for database-connection (postgresql)
+print("Connecting to database...")
+conn = psycopg2.connect(
+    host="localhost",
+    database="db_gui",
+    user="postgres",
+    password="master",
+    port="5434")
+print("Successfully connected to database!")
+
+# Define mqtt-settings.
+MQTT_Broker = "158.38.66.180"
+MQTT_Port = 1883
+MQTT_Username = 'ntnu'
+MQTT_Password = 'ntnuais2103'
+MQTT_Keepalive = 5
+
+# Define mqtt-topics
+# subscribe
+MQTT_topic_L160_UnfilteredTemp = 'NTNU/IoT/Group13/L160/UnfilteredTemp'
+MQTT_topic_L160_FilteredTemp = 'NTNU/IoT/Group13/L160/FilteredTemp'
+
+# Define mqtt-client
+mqtt_client = mqtt.Client()
+mqtt_client.username_pw_set(username=MQTT_Username, password=MQTT_Password)
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Successfully connected to mqtt-broker!")
+        mqtt_client.subscribe(MQTT_topic_L160_UnfilteredTemp)
+        mqtt_client.subscribe(MQTT_topic_L160_FilteredTemp)
+        # Add more subscriptions here
+    else:
+        print(f"Bad connection. Code: {rc}")
+        
+def on_message(client, userdata, _msg):
+    # ======= DECODE MESSAGE =======
+    msg = dict(
+        topic=_msg.topic,
+        payload=_msg.payload.decode().split(',')
+    )
+    print(msg)
 
 class MQTT:
-    def __init__(self, topic):
+    def __init__(self, topic1):
         self.topic = topic
+# Sette opp init neste gang HER!
 
 
-    mqtt_client = Mqtt(app)
+
+'''
     @app.route('/', methods=['POST', 'GET'])
     @mqtt_client.on_connect()
     def handle_connect(client, userdata, flags, rc):
@@ -52,8 +87,7 @@ class MQTT:
                 password="Elias123")
             sql = """INSERT INTO temperature(temp, timestamp) VALUES(%s,%s);"""
             try:
-     7
-                 cur = conn.cursor()
+                cur = conn.cursor()
                 cur.execute(sql, (data['payload'], dt.now()))
                 conn.commit()
                 cur.close()
@@ -63,17 +97,16 @@ class MQTT:
             try:
                 cur = conn.cursor()
                 cur.execute(sql)
-                tempFromDB = pd.DataFrame(cur.fetchall(), columns=['id', 'temp',
-    􏰀→ 'timestamp']) # Filter 1
-    order = 2
-    f=1
-    cut = 0.1
+                tempFromDB = pd.DataFrame(cur.fetchall(), columns=['id', 'temp','timestamp']) # Filter 1
+                order = 2
+                f=1
+                cut = 0.1
                 nyq = 0.5 * f
                 cutoff = cut / nyq
                 b, a = butter(order, cutoff, btype='low', analog=False)
-    filtTemp = filtfilt(b, a, tempFromDB["temp"]) cur.execute(sql, (dt.now(),filtTemp))
-    # Filter 2
-    temp = 0
+                filtTemp = filtfilt(b, a, tempFromDB["temp"]) cur.execute(sql, (dt.now(),filtTemp))
+                # Filter 2
+                temp = 0
                 for temperature in filtTemp:
                     temp += temperature
                 temp = temp / len(filtTemp)
@@ -102,7 +135,6 @@ class MQTT:
     topic1 = 'RTUIot/Ard1/BMP280/Temp'
     topic2 ='NTNU/Miniproj/Elias/FiltTemp'
     topic3 ='RTUIot/flask/data'
-     8
     
      mqtt_client = Mqtt(app)
     @app.route('/', methods=['POST', 'GET'])
@@ -141,43 +173,118 @@ class MQTT:
                     conn.close()
     if __name__ == '__main__':
         app.run(host='127.0.0.1', port=5001)
-    C Database initializing code
-    version: '3.8'
-    services:
-      postgresdb1:
-        container_name: ntnu_db1
-        image: postgres
-        restart: always
-        environment:
-          POSTGRES_PASSWORD: Elias123
-          POSTGRES_USER: user
-          POSTGRES_DB: IOTDB1
-        volumes:
-          - ./data/dbfiles1:/var/lib/postgresql/data
-     9
-    
-       ports:
-        - 5432:5432
-    postgresdb2:
-      container_name: ntnu_db2
-      image: postgres
-      restart: always
-      environment:
-        POSTGRES_PASSWORD: Elias123
-        POSTGRES_USER: user
-        POSTGRES_DB: IOTDB2
-      volumes:
-        - ./data/dbfiles2:/var/lib/postgresql/data
-      ports:
-        - 5433:5432
-    pgadmin:
-      container_name: pgadmin4
-      image: dpage/pgadmin4
-      restart: always
-      environment:
-        PGADMIN_DEFAULT_EMAIL: user@ntnu.no
-        PGADMIN_DEFAULT_PASSWORD: ntnu
-      ports:
-        - "8080:80"
-      volumes:
-        - ./data/pgadmin:/var/lib/pgadmin
+'''
+        
+
+
+
+'''
+import paho.mqtt.client as mqtt
+import psycopg2
+
+# Define database-connection (postgresql)
+print("Connecting to database...")
+conn = psycopg2.connect(
+    host="localhost",
+    database="db_gui",
+    user="postgres",
+    password="master",
+    port="5434")
+print("Successfully connected to database!")
+
+# Define mqtt-settings.
+MQTT_BROKER = "158.38.67.229"
+MQTT_PORT = 1883
+MQTT_USERNAME = 'ntnu'
+MQTT_PASSWORD = 'ntnuais2103'
+MQTT_KEEPALIVE = 5
+
+# Define mqtt-topics
+# subscribe
+topic_temperature_filtered = 'RTUIot/marsi/Temp/filtered'
+topic_pressure_filtered = 'RTUIot/marsi/Press/filtered'
+
+topic_temperature_aggr = 'RTUIot/marsi/Temp/aggr'
+topic_pressure_aggr = 'RTUIot/marsi/Press/aggr'
+
+# Define mqtt-client
+mqtt_client = mqtt.Client()
+mqtt_client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
+
+
+# Define "on_connect"-function
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Successfully connected to mqtt-broker!")
+        mqtt_client.subscribe(topic_temperature_filtered)
+        mqtt_client.subscribe(topic_pressure_filtered)
+
+        mqtt_client.subscribe(topic_temperature_aggr)
+        mqtt_client.subscribe(topic_pressure_aggr)
+    else:
+        print(f"Bad connection. Code: {rc}")
+
+
+# Define "on_message"-function
+def on_message(client, userdata, _msg):
+    # ======= DECODE MESSAGE =======
+    msg = dict(
+        topic=_msg.topic,
+        # payload[0] = time or date, payload[1] = value
+        payload=_msg.payload.decode().split(',')
+    )
+    print(msg)
+
+    query = None
+
+    # ============== TEMPERATURE ==================
+    if msg['topic'] == topic_temperature_filtered:
+        query = f"""INSERT INTO temperature_log (timestamp, value) VALUES(%s, %s);"""
+    elif msg['topic'] == topic_pressure_filtered:
+        query = f"""INSERT INTO pressure_log (timestamp, value) VALUES(%s, %s);"""
+    elif msg['topic'] == topic_temperature_aggr:
+        query = f"""MERGE INTO temperature_aggr AS T
+	                USING (SELECT '{msg['payload'][0]}'::date AS date, {msg['payload'][1]} AS value_max, {msg['payload'][2]} AS 
+                            value_min, {msg['payload'][3]} AS value_avg 
+                            ) AS S 
+                    ON (T.date = S.date)
+                    WHEN MATCHED THEN 
+                        UPDATE SET 
+                            value_max = S.value_max,
+                            value_min = S.value_min, 
+                            value_avg = S.value_avg  
+                    WHEN NOT MATCHED THEN 
+                        INSERT (date, value_min, 
+                            value_max, value_avg) VALUES (S.date, S.value_min, S.value_max, S.value_avg);"""
+    elif msg['topic'] == topic_pressure_aggr:
+        query = f"""MERGE INTO pressure_aggr AS T
+        	                USING (SELECT '{msg['payload'][0]}'::date AS date, {msg['payload'][1]} AS value_max, {msg['payload'][2]} AS 
+                                    value_min, {msg['payload'][3]} AS value_avg 
+                                    ) AS S 
+                            ON (T.date = S.date)
+                            WHEN MATCHED THEN 
+                                UPDATE SET 
+                                    value_max = S.value_max,
+                                    value_min = S.value_min, 
+                                    value_avg = S.value_avg  
+                            WHEN NOT MATCHED THEN 
+                                INSERT (date, value_min, 
+                                    value_max, value_avg) VALUES (S.date, S.value_min, S.value_max, S.value_avg);"""
+
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query, msg['payload'])
+            conn.commit()
+    except psycopg2.DatabaseError as error:
+        print(error)
+        return
+
+# Connect to mqtt-broker
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+print("Connecting to mqtt-broker...")
+mqtt_client.connect(MQTT_BROKER, MQTT_PORT, MQTT_KEEPALIVE)
+
+if __name__ == '__main__':
+    mqtt_client.loop_forever()
+'''
