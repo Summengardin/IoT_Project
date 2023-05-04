f68fb3d2dd1388570c9fa2822e7e813857e60844
EliasWR <eliaswoierefsdal@gmail.com>
2023-05-04 11:46:44 +0200

    Adjustments to TCP comms

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import psycopg2
 
 from src import aranet_handling
-from src import mqtt_handling
 
 # ============ API ===============
 # API BASE URL: https://aranet.cloud/api/
@@ -12,23 +11,9 @@ from src import mqtt_handling
 api_key = "drfk8t5k6tb576ndykq65wwyn3tee3b4" # NTNU
 #api_key = "sut582nzqzc8c6utvryxvqecucmbb5dg" # RTU
 
-# ============ MQTT ==============
-# Define mqtt-settings.
-MQTT_Broker = "158.38.66.180"
-MQTT_Port = 1883
-MQTT_Username = 'ntnu'
-MQTT_Password = 'ntnuais2103'
-MQTT_Keepalive = 5
-
-# Define mqtt-topics
-topic1 = 'NTNU/IoT/Group13/L160/UnfilteredTemp'
-topic2 = 'NTNU/IoT/Group13/L160/UnfilteredPressure'
-topic3 = 'NTNU/IoT/Group13/L160/UnfilteredCo2'
-
 def main():
     ARA = aranet_handling.ARANET(api_key = api_key)
     ARA.list_sensor_names()
-    MQTT = mqtt_handling.MQTT_TO_PG(MQTT_Username, MQTT_Password, MQTT_Port, MQTT_Broker,MQTT_Keepalive)
 
 if __name__ == '__main__':
     main()
