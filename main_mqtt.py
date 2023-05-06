2b5bbea0a3f7b77e00caeaa53f83892f2db16c6f
EliasWR <eliaswoierefsdal@gmail.com>
2023-05-06 20:55:55 +0200

    Added arduino_V2

--- main_mqtt.py
+++ main_mqtt.py
@@ -2,15 +2,6 @@ import psycopg2
 
 from src import mqtt_handling
 
-# ============ API ===============
-# API BASE URL: https://aranet.cloud/api/
-# API KEY (NTNU): "drfk8t5k6tb576ndykq65wwyn3tee3b4"
-# API KEY (RTU):  "sut582nzqzc8c6utvryxvqecucmbb5dg"
-# API NAME: "https://aranet.cloud/openapi/#/"
-
-api_key = "drfk8t5k6tb576ndykq65wwyn3tee3b4" # NTNU
-#api_key = "sut582nzqzc8c6utvryxvqecucmbb5dg" # RTU
-
 # ============ MQTT ==============
 # Define mqtt-settings.
 MQTT_Broker = "158.38.66.180"
@@ -20,10 +11,10 @@ MQTT_Password = 'ntnuais2103'
 MQTT_Keepalive = 5
 
 # Define mqtt-topics
-topic1 = 'Gruppe_23_2/Elias/sensordata'
-topic2 = 'Gruppe_23_2/Mathieu/sensordata'
-topic3 = 'Gruppe_23_2/Melina/sensordata'
-topic4 = 'Gruppe_23_2/Martin/sensordata'
+topic1 = 'NTNU/Group23_2/Room1/Sensordata'
+topic2 = 'NTNU/Group23_2/Room2/Sensordata'
+topic3 = 'NTNU/Group23_2/Room1/Sensordata'
+topic4 = 'NTNU/Group23_2/Room1/Sensordata'
 
 if __name__ == '__main__':
     MQTT = mqtt_handling.MQTT_TO_PG(MQTT_Username, MQTT_Password, MQTT_Port, MQTT_Broker,MQTT_Keepalive)
