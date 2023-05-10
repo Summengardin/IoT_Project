e8ad0533067661b348c3fc33ec552f79019b516c
MartinSimengard <simengardmartin@gmail.com>
2023-05-10 16:06:04 +0200

    Uncomment

--- src/postgres_handling.py
+++ src/postgres_handling.py
@@ -27,26 +27,26 @@ class Database():
         data = df.to_dict(orient='records')
         # ============== INSERTING SENSOR VALUES ==================
         try:
-#            with self.conn.cursor() as cur:
-            for row in data:
-                # Get the table name for the current row based on sensorid
-                sensor_id = row['sensorid']
+            with self.conn.cursor() as cur:
+                for row in data:
+                    # Get the table name for the current row based on sensorid
+                    sensor_id = row['sensorid']
 
-                table_name = table_names[sensor_id]
-                
-                if table_name is not None:
-                    # Insert the current row into the corresponding table
-                    press = row['Atmospheric Pressure']
-                    co2 = row['CO₂']
-                    humid = row['Humidity']
-                    temp = row['Temperature']
-                    timestamp = row['datetime'].to_pydatetime().replace(tzinfo=None)
+                    table_name = table_names[sensor_id]
+                    
+                    if table_name is not None:
+                        # Insert the current row into the corresponding table
+                        press = row['Atmospheric Pressure']
+                        co2 = row['CO₂']
+                        humid = row['Humidity']
+                        temp = row['Temperature']
+                        timestamp = row['datetime'].to_pydatetime().replace(tzinfo=None)
 
-                    insert_row_sql = f"INSERT INTO {table_name} (pressure, co2_equivalent, humidity, temperature, time) VALUES (%s, %s, %s, %s, %s);"
-                    print(insert_row_sql, (press, co2, humid, temp, timestamp) )
-                    # cur.execute(insert_row_sql, tuple(row.values[1:]))
+                        insert_row_sql = f"INSERT INTO {table_name} (pressure, co2_equivalent, humidity, temperature, time) VALUES (%s, %s, %s, %s, %s);"
+                        #print(insert_row_sql, (press, co2, humid, temp, timestamp) )
+                        cur.execute(insert_row_sql, (press, co2, humid, temp, timestamp))
     
-#            cur.commit()
+            cur.commit()
 
         except psycopg2.DatabaseError as e:
             print(f'Error inserting values into {table_name}: {e}')
